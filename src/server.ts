275465780c2933ad7a317c7ee545bcd2071bc597
Afeez Adeboye <afeezadeboyee@gmail.com>
2022-06-19 20:09:50 +0100

    This is my third commit

--- src/server.ts
+++ src/server.ts
@@ -13,42 +13,22 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
-  // GET /filteredimage?image_url={{URL}}
   app.get( "/filteredimage/", async (req:express.Request, res:express.Response) => {
-    // extract image_url from request query
+    // This is to extract the image_url from request query
     const image_url:string = req.query.image_url
       if(!image_url){
-        return res.status(400).send("image_url is missing")
+        return res.status(400).send("This image_url is missing")
       }
       try {
-    // call filterImageFromURL(image_url) to filter the image and https://sourceforge.net/projects/unxutils/
+    // calling the filterImageFromURL(image_url) to filter the image and to the https://sourceforge.net/projects/unxutils/
         const imagePath= await filterImageFromURL(image_url)
         return res.status(200).sendFile(imagePath, async (error)=>{
           await deleteLocalFiles ([imagePath])
         });
           } catch (error) {
-    // send the resulting file in the response
-        return res.status(422).send("image url cannot be processed")
+    // sending the resulting file in the response
+        return res.status(422).send("This image_url cannot be processed")
       }
     } );
   app.get( "/", async ( req, res ) => {
